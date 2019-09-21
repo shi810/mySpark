b83f1d0606ce99b5265500c2354de03764fa6e57
begin <1621695162@qq.com>
2019-09-21 15:41:11 +0800

    Initial commit

---
+++ src/main/scala/sql/CreateDFFromJsonFile.scala
@@ -0,0 +1,31 @@
+package sql
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession}
+
+/**
+  * Created by 史红光 on 2019/9/21.
+  */
+object CreateDFFromJsonFile {
+    def main(args: Array[String]): Unit = {
+
+        val spark = SparkSession
+                .builder()
+                .master("local")
+                .appName("jsonRDD")
+                .getOrCreate()
+        val jsonDS: DataFrame = spark.read.json("./data/json")
+
+        jsonDS.show()
+
+        jsonDS.createOrReplaceTempView("jsonTable")
+
+        val result: DataFrame = spark.sql("select * from jsonTable where age is not NULL")
+
+        result.show()
+
+        spark.stop()
+
+    }
+
+}
